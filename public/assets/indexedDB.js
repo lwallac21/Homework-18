960d3246333ce11baaa91ff0017367510c1684e8
Lawrence Wallace <lwallac21@gmail.com>
2021-01-12 15:40:40 -0500

    created object store called pending

--- public/assets/indexedDB.js
+++ public/assets/indexedDB.js
@@ -1,2 +1,21 @@
 let db;
 const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+    // create object store called "pending" and set autoIncrement to true
+   const db = event.target.result;
+   db.createObjectStore("pending", { autoIncrement: true });
+ };
+ 
+ request.onsuccess = function(event) {
+   db = event.target.result;
+ " " "
+   if (navigator.onLine) {
+     checkDatabase();
+   }
+ };
+ 
+ request.onerror = function(event) {
+   console.log(event.target.errorCode);
+ };
+ 
