d3c24fc7e8d600497adcf206052f7fcf604da005
Lawrence Wallace <lwallac21@gmail.com>
2021-01-13 13:59:39 -0500

    added mongo db env variable for mongoose connection

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+const URL = process.env.MONGODB_URI || "mongodb://localhost/budget"
+
+mongoose.connect(URL, {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+})
+
 
 // routes
 app.use(require("./routes/api.js"));
